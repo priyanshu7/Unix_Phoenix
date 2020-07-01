c57594fdf82147e16343d14b8f13cab793417c1e
Priyanshu Mehta <mehta7priyanshu@gmail.com>
2020-07-01 14:50:42 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,36 +1,38 @@
-num_file=$(ls | wc -l)
+num_file=$(pwd | ls -1A | wc -l)
 
-function guess_game {		
-	read input
-	re='^[0-9]+$'
-	if ! [[ $input =~ $re ]]
-	then
-   		echo "Error: Not a valid input"
-		echo "Enter a number"
-		guess_game
-	fi
-	if [[ $input -eq $num_file ]]
-	then
-		echo "CONGRATULATIONS :) You guessed it! Wohhooooooo"
-		echo "  Following files are present in directory >>>>-->>>>"
-		for file in $(ls)
-		do
-			echo "  - $file , "
-		done
-		echo "You have successfully finished off the game! WINNER !!!"
-		echo "Do play again in some time!"
-	elif [[ $input -gt $num_file ]]
-	then
-		echo "SORRY! That's a wrong answer"
-		echo "Too High"
-		echo "Hint: Try a smaller number, Guess NOW"
-		guess_game
-	else
-		echo "SORRY! That's a wrong answer"
-		echo "Too Low"
-		echo "Hint: Try a bigger number, GUESS NOW"
-		guess_game
-	fi
+function guess_game {
+	while true
+	do
+		read input
+		re='^[0-9]+$'
+		if ! [[ $input =~ $re ]]
+		then
+   			echo "Error: Not a valid input"
+			echo "Enter a number"
+			continue
+		fi
+		if [[ $input -eq $num_file ]]
+		then
+			echo "CONGRATULATIONS :) You guessed it! Wohhooooooo"
+			echo "  Following files are present in directory >>>>-->>>>"
+			for file in $(ls -1A)
+			do
+				echo "  - $file , "
+			done
+			echo "You have successfully finished off the game! WINNER !!!"
+			echo "Do play again in some time!"
+			break
+		elif [[ $input -gt $num_file ]]
+		then
+			echo "SORRY! That's a wrong answer"
+			echo "Too High"
+			echo "Hint: Try a smaller number, Guess NOW"
+		else
+			echo "SORRY! That's a wrong answer"
+			echo "Too Low"
+			echo "Hint: Try a bigger number, GUESS NOW"
+		fi
+	done
 }
 echo "Cat has 9 lives! But you get more, anyways try finishing in less tries."
 echo "Can you guess how many files are there in the current directory? Bring your A game"
